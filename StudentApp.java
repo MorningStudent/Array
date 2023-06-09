9bfe3dcfd959c8d97e699a84730488f20d9298f7
VictorCorlateanu <126675107+VictorCorlateanu@users.noreply.github.com>
2023-06-09 19:40:04 +0300

    Add files via upload
    This version of StudentApp completes the logic for CRUD operation and adds an interactive Menu function.

--- StudentApp.java
+++ StudentApp.java
@@ -2,22 +2,36 @@ import java.util.Scanner;
 
 public class StudentApp {
     public static void main(String[] args) {
-        
-        StudentDatabase.students = new Student[3];
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter student's 1 data: ");
-        StudentDatabase.addStudent(in.nextLine(), in.nextLine(), in.nextInt()); //it leaves an "\n" that is captured by the next nextLine()
-        in.nextLine();
-        System.out.println("Enter student's 2 data: ");
-        StudentDatabase.addStudent(in.nextLine(), in.nextLine(), in.nextInt());
-        in.nextLine();
-        System.out.println("Enter student's 3 data: ");
-        StudentDatabase.addStudent(in.nextLine(), in.nextLine(), in.nextInt());
-        in.nextLine();
-        System.out.println("Enter student's 4 data: ");
-        StudentDatabase.addStudent(in.nextLine(), in.nextLine(), in.nextInt());
-        in.nextLine();
-        StudentDatabase.printAllStudents();
+        StudentDatabase.students = new Student[4];
+        int option;
+        while (true) {
+            option = ConsoleUI.printMenu();
+            switch (option) {
+                case 1: StudentDatabase.printAllStudents(); break;
+                case 2: System.out.println("Enter student's Full Name, Group Name, General Grade to be added: ");
+                        StudentDatabase.addStudent(in.nextLine(), in.nextLine(), in.nextInt());
+                        in.nextLine(); break;
+                case 3: System.out.println("Enter student's Full Name, Group Name, General Grade, Index to be updated: ");
+                        StudentDatabase.updateStudent(in.nextLine(), in.nextLine(), in.nextInt(), in.nextInt());
+                        in.nextLine(); break;
+                case 4: System.out.println("Enter student's Index to be deleted: ");
+                        StudentDatabase.deleteStudent(in.nextInt());
+                        in.nextLine(); break;
+                default: System.out.println("No such option!");
+            }
+        }
+    }
+}
+
+class ConsoleUI {
+    public static int printMenu() {
+        System.out.println("MENU");
+        System.out.println("1. To view students list\n2. To add student\n3. To update student\n4. To delete student");
+        System.out.print(">> ");
+        Scanner in = new Scanner(System.in);
+        int option = in.nextInt();
+        return option;
     }
 }
 
@@ -26,23 +40,53 @@ class StudentDatabase {
     public static int lastIndex = 0;
 
     public static void addStudent(String fullName, String groupName, int generalGrade) {
-        if (lastIndex != students.length) {
-            students[lastIndex] = new Student(fullName, groupName, generalGrade);
-            lastIndex++;
-        } else if (lastIndex == students.length) {
-            System.err.println("Warrning! Only " + students.length + " students can be stored!");
+        Student[] newStudents = new Student[students.length+1];
+        for (int i=0, j=0; i<students.length; i++, j++) {
+            newStudents[j] = students[i];
         }
+        students = newStudents;
+        students[lastIndex] = new Student(fullName, groupName, generalGrade);
+        lastIndex++;
     }
 
     public static void printAllStudents() {
         System.out.println("\n\nSTUDENT LIST: ");
         for (Student student : students) {
-            if (student == null) {      //<--cannot use here .equals, it doesn't work when "student" is null, but "==" is fine.
+            if (student == null) {    
                 
             } else {
                 System.out.println(student);
             }
         }
+    }
+
+    public static void updateStudent(String fullName, String groupName, int generalGrade, int index) {
+        students[index].setFullName(fullName);
+        students[index].setGroupName(groupName);
+        students[index].setGeneralGrade(generalGrade);
+    }
+
+    public static void updateStudent(String fullName, String groupName, int index) {
+        students[index].setFullName(fullName);
+        students[index].setGroupName(groupName);
+    }
+
+    public static void updateStudent(String fullName, int index) {
+        students[index].setFullName(fullName);
+    }
+
+    public static void updateStudent(int index, int generalGrade) {
+        students[index].setGeneralGrade(generalGrade);
+    }
+
+    public static void deleteStudent(int index) {
+        Student[] newStudents = new Student[students.length-1];
+        for (int i=0, j=0; i<students.length; i++) {
+            if (i != index) {
+                newStudents[j++] = students[i];
+            }
+        }
+        students = newStudents;
     }
 }
 
@@ -86,5 +130,4 @@ class Student {
     public String toString() {
         return "Student [fullName=" + fullName + ", groupName=" + groupName + ", generalGrade=" + generalGrade + "]";
     }
-
 }
